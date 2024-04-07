917fbe0438189af25304aafc73480b9594a06296
Robert Crabtree <robert.crabtree@gmail.com>
2024-04-07 05:12:11 -0700

    Clean up

--- AccessibilityDemoUITests/AccessibilityDemoUITests.swift
+++ AccessibilityDemoUITests/AccessibilityDemoUITests.swift
@@ -20,15 +20,23 @@ final class AccessibilityDemoUITests: XCTestCase {
     func testLoginFlow() {
         app.buttons[OnboardingAccessibility.loginButton].tap()
 
+        // Wait for login view to appear
         XCTAssertTrue(app.textFields[LoginAccessibility.emailTextField].waitForExistence(timeout: 5))
+
         app.textFields[LoginAccessibility.emailTextField].tap()
         app.textFields[LoginAccessibility.emailTextField].typeText("accessibility@test.com")
+
         app.secureTextFields[LoginAccessibility.passwordTextField].tap()
         app.secureTextFields[LoginAccessibility.passwordTextField].typeText("astrongpassword")
+
+        // dismiss the keyboard so loginButton is visible
         app.otherElements[LoginAccessibility.view].tap()
+
         app.buttons[LoginAccessibility.loginButton].tap()
 
+        // wait for home view to appear
         XCTAssertTrue(app.buttons[HomeAccessibility.logoutButton].waitForExistence(timeout: 5))
+
         app.buttons[HomeAccessibility.logoutButton].tap()
     }
 }
